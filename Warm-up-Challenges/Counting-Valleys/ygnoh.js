0871939ee10f18f95dfaac0e2085985e6e4ac2b3
yonggoo.noh <yonggoo.noh@navercorp.com>
2019-01-15 20:32:39 +0900

    Practice > Interview Preparation Kit > Warm-up Challenges > Counting Valleys

---
+++ Warm-up-Challenges/Counting-Valleys/ygnoh.js
@@ -0,0 +1,66 @@
+/**
+ * https://www.hackerrank.com/challenges/counting-valleys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+ *
+ * space complexity: O(n) / time complexity: O(n)
+ */
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    let level = 0;
+    let valleyCount = 0;
+    let stepCount;
+
+    for (stepCount = 0; stepCount < n; stepCount++) {
+        if (s[stepCount] === "U") {
+            if (level === -1) {
+                valleyCount++;
+            }
+
+            level++;
+        } else {
+            level--;
+        }
+    }
+
+    return valleyCount;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
