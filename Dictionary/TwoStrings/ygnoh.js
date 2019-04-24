3165a54ffcc833283c920736b894fb3220808fda
Yonggoo Noh <yonggoo.noh@navercorp.com>
2019-04-24 13:00:28 +0900

    Update ygnoh.js

--- Dictionary/TwoStrings/ygnoh.js
+++ Dictionary/TwoStrings/ygnoh.js
@@ -1,1 +1,8 @@
+function twoStrings(s1, s2) {
+    const base = {};
+    [...s1].forEach(char1 => {
+        base[char1] = true;
+    });
 
+    return [...s2].some(char2 => base[char2]) ? "YES" : "NO";
+}
